a9dd1bcc56702cc98da0da68b4f2d0078a6326eb
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-05-02 17:22:18 -0700

    added feature to fetch and display country flag and info

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,57 +1,94 @@
+import React, { useState, useEffect, Fragment } from 'react'
 import { HomeScreen } from 'app/features/home/screen'
-import { Fragment } from 'react'
 
 export default function Home() {
+  const [countriesData, setCountriesData] = useState({
+    list: [],
+    searchVal: '',
+    filtered: [],
+  })
+
+  //fetches data for countries
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://restcountries.com/v3.1/independent?status=true'
+      )
+      const data = await response.json()
+      setCountriesData({ ...countriesData, list: data })
+    } catch (err) {
+      throw err
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    console.table(countriesData.list)
+  }, [countriesData])
+
   return (
-    <>
-      <header className="">
-        <div className="mx-auto flex w-11/12 justify-between">
+    <div className="pt-8">
+      <header className="pb-8">
+        <div className="mx-auto flex w-11/12 items-center justify-between">
           <h1>Where in the world?</h1>
           <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white">
             button
           </button>
         </div>
       </header>
-      <div>
+      <div className="mx-auto w-11/12">
         <div>
           <form onSubmit={(e) => e.preventDefault()}>
             <input
-              className="block w-full rounded-lg p-2.5 text-sm shadow-lg"
-              placeholder="Search for a country"
+              className="block rounded-lg bg-blue-300 p-2.5 text-sm"
+              placeholder="Search for a country..."
+              list="region"
             />
-
-            <div>
-              <div className="mt-2">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-2"
-                >
-                  <option>Africa</option>
-                  <option>America</option>
-                  <option>Asia</option>
-                  <option>Europe</option>
-                  <option>Oceania</option>
+            <div className="flex mb-8">
+              <div className="mt-10">
+                <select id="region" className="">
+                  <option value="" disabled>
+                    Filter by region
+                  </option>
+                  <option value="Africa">Africa</option>
+                  <option value="America">America</option>
+                  <option value="Asia">Asia</option>
+                  <option value="Europe">Europe</option>
+                  <option value="Oceania">Oceania</option>
                 </select>
               </div>
             </div>
           </form>
         </div>
-        <div>
-          <div className="max-w-sm rounded ">
-            {/* <img className="w-full" src="" alt="Flag" /> */}
-            <div className="px-8 py-4">
-              <div className="mb-2 text-xl font-bold">Country</div>
-              <p className="text-base text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
-            </div>
-          </div>
+        <div className="">
+          {countriesData?.list?.map((country: any, index: number) => {
+            return (
+              <div className="max-w-sm rounded mx-auto mb-8 " key={`react-key${index}`}>
+                <img
+                  className="w-full"
+                  src={country.coatOfArms.png}
+                  alt="Flag"
+                />
+                <div className="px-8 py-4">
+                  <p className="mb-2 text-xl font-bold">{country.altSpellings[1]}</p>
+                  <p className="text-base text-gray-700">
+                    Population: {country.population}
+                  </p>
+                  <p className="text-base text-gray-700">
+                    Region: {country.region}
+                  </p>
+                  <p className="text-base text-gray-700">
+                    Capital: {country.capital[0]}
+                  </p>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
-    </>
+    </div>
   )
 }
